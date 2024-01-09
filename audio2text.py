0906acd0ac4fefec1687d161d679dac545e8748b
zonbloop <71484497+zonbloop@users.noreply.github.com>
2024-01-08 18:16:18 -0600

    Add files via upload

--- audio2text.py
+++ audio2text.py
@@ -1,87 +1,94 @@
-from pydub import AudioSegment
-import sys
-import whisper
-import datetime
 import subprocess
 import torch
-import pyannote.audio
+import whisper
+import datetime
 from pyannote.audio.pipelines.speaker_verification import PretrainedSpeakerEmbedding
-embedding_model = PretrainedSpeakerEmbedding(
-    "speechbrain/spkrec-ecapa-voxceleb",
-    device=torch.device("cuda"))
 from pyannote.audio import Audio
 from pyannote.core import Segment
 import wave
 import contextlib
-from sklearn.cluster import AgglomerativeClustering
 import numpy as np
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.preprocessing import MinMaxScaler
+from scipy.io import wavfile
+import noisereduce as nr
+from pydub import AudioSegment
 import locale
-
-def segment_embedding(segment):
-  start = segment["start"]
-  # Whisper overshoots the end timestamp in the last segment
-  end = min(duration, segment["end"])
-  clip = Segment(start, end)
-  waveform, sample_rate = audio.crop(path, clip)
-  return embedding_model(waveform[None])
-
-def time(secs):
-  return datetime.timedelta(seconds=round(secs))
-# Cargar el archivo de audio estéreo
-path = sys.argv[1]
-speakers = sys.argv[2]
-lang = sys.argv[3]
-
-# Convertir a mono
-audio_estereo = AudioSegment.from_file(path)
-audio_mono = audio_estereo.set_channels(1)
-
-# Guardar el audio en formato mono
-audio_mono.export(path, format="mp3")
-
-# Set Number of speakers, Language & model
-num_speakers = speakers #@param {type:"integer"}
-language = lang #@param ["any", "English", "Dutch"]
-model_size = "large-v2" #@param ["tiny", "base", "small", "medium", "large", "large-v2"]
-
-model_name = model_size
-if language == 'English' and model_size != 'large':
-  model_name += '.en'
-
-if path[-3:] != 'wav':
-  subprocess.call(['ffmpeg', '-i', path, 'audio.wav', '-y'])
-  path = 'audio.wav'
-  model = whisper.load_model(model_size)
-
 locale.getpreferredencoding = lambda: "UTF-8"
-model = whisper.load_model(model_size)
 
-result = model.transcribe(path)
-segments = result["segments"]
-subprocess.call(['ffmpeg', '-i', path, 'mono.wav','-y'])
+class TranscriptionProcessor:
+    def __init__(self, num_speakers=2, language="any", model_size="large-v2"):
+        self.num_speakers = num_speakers
+        self.language = language
+        self.model_size = model_size
+        self.audio = Audio()
+        self.embedding_model = PretrainedSpeakerEmbedding(
+            #"speechbrain/spkrec-ecapa-voxceleb",
+            "speechbrain/asr-crdnn-commonvoice-14-es",
+            device=torch.device("cuda")
+        )
+        self.model = whisper.load_model(self.model_size)
 
-with contextlib.closing(wave.open('mono.wav','r')) as f:
-  frames = f.getnframes()
-  rate = f.getframerate()
-  duration = frames / float(rate)
+    def convert_to_mono(self):
+        audio_estereo = AudioSegment.from_file(self.audio_path)
+        audio_mono = audio_estereo.set_channels(1)
+        audio_mono.export("uploaded_files/mono.wav", format="wav")
 
-audio = Audio()
+    def reduce_noise(self):
+        rate, data = wavfile.read("uploaded_files/mono.wav")
+        reduced_noise = nr.reduce_noise(y=data, sr=rate)
+        wavfile.write("uploaded_files/mono_reduced.wav", rate, reduced_noise)
 
-embeddings = np.zeros(shape=(len(segments), 192))
-for i, segment in enumerate(segments):
-  embeddings[i] = segment_embedding(segment)
+    def load_whisper_model(self):
+        subprocess.call(['ffmpeg', '-i', 'uploaded_files/mono_reduced.wav', 'uploaded_files/mono.wav', '-y'])
+        result = self.model.transcribe("uploaded_files/mono_reduced.wav")
+        self.segments = result["segments"]
 
-embeddings = np.nan_to_num(embeddings)
+    def transcribe_audio(self):
+        frames, rate, duration = self.get_audio_info('uploaded_files/mono_reduced.wav')
+        self.embeddings = np.zeros(shape=(len(self.segments), 192))
+        for i, segment in enumerate(self.segments):
+            self.embeddings[i] = self.segment_embedding(segment, duration)
 
-clustering = AgglomerativeClustering(num_speakers).fit(embeddings)
-labels = clustering.labels_
-for i in range(len(segments)):
-  segments[i]["speaker"] = 'SPEAKER ' + str(labels[i] + 1)
+        self.embeddings = np.nan_to_num(self.embeddings)
 
-f = open("output_files/transcript.txt", "w", encoding="utf-8")
+    def segment_embedding(self, segment, duration):
+        start = segment["start"]
+        end = min(duration, segment["end"])
+        clip = Segment(start, end)
+        waveform, _ = self.audio.crop('uploaded_files/mono_reduced.wav', clip)
+        return self.embedding_model(waveform[None])
 
-for (i, segment) in enumerate(segments):
-  if i == 0 or segments[i - 1]["speaker"] != segment["speaker"]:
-    f.write("\n" + segment["speaker"] + ' ' + str(time(segment["start"])) + '\n')
-  f.write(segment["text"][1:] + ' ')
-f.close()
+    def cluster_speakers(self):
+        clustering = AgglomerativeClustering(
+            self.num_speakers, metric='euclidean', linkage='ward'
+        ).fit(MinMaxScaler().fit_transform(self.embeddings))
+        self.labels = clustering.labels_
+        for i in range(len(self.segments)):
+            self.segments[i]["speaker"] = 'SPEAKER ' + str(self.labels[i] + 1)
+
+    def save_transcription(self):
+        with open("output_files/transcript.txt", "w", encoding="utf-8") as f:
+            for (i, segment) in enumerate(self.segments):
+                if i == 0 or self.segments[i - 1]["speaker"] != segment["speaker"]:
+                    f.write("\n" + segment["speaker"] + ',' + str(self.time(segment["start"])) + ',')
+                f.write(segment["text"][1:] + ' ')
+
+    def time(self, secs):
+        return datetime.timedelta(seconds=round(secs))
+
+    def get_audio_info(self, filepath):
+        with contextlib.closing(wave.open(filepath, 'r')) as f:
+            frames = f.getnframes()
+            rate = f.getframerate()
+            duration = frames / float(rate)
+        return frames, rate, duration
+
+    def run_transcription(self,audio_path):
+        self.audio_path = audio_path
+        self.convert_to_mono()
+        self.reduce_noise()
+        self.load_whisper_model()
+        self.transcribe_audio()
+        self.cluster_speakers()
+        self.save_transcription()
